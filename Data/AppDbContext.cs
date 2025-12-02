a38a75b89169ffe1d4727c594f655b111bdef680
Julian <jscristancho83@gmail.com>
2025-12-01 20:07:58 -0500

    added connection string to context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -34,7 +34,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("\"Host=db.faovmwzwrmqedyiisecx.supabase.co;Database=postgres;Username=postgres;Password=asE4RDF0hG8zTNsb;SSL Mode=Require;Trust Server Certificate=true\"");
+        => optionsBuilder.UseNpgsql("Host=db.faovmwzwrmqedyiisecx.supabase.co;Database=postgres;Username=postgres;Password=asE4RDF0hG8zTNsb;SSL Mode=Require;Trust Server Certificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
